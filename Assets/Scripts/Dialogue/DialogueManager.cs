1b8567a22e53bb504940b6b934790d089d003254
Mr Plenty <allan_leeman20@hotmail.co.uk>
2025-07-16 17:58:52 +0100

    Stated adding text

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private TextMeshProUGUI textComponent;
+
     void Start()
     {
-        
+        GameObject canvasGO = new GameObject("Canvas");
+        Canvas canvas = canvasGO.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvasGO.AddComponent<CanvasScaler>();
+        canvasGO.AddComponent<GraphicRaycaster>();
+
+        GameObject textGO = new GameObject("Text");
+        textGO.transform.SetParent(canvasGO.transform);
+
+        textComponent = textGO.AddComponent<TextMeshProUGUI>();
+        textComponent.text = "Hello, Doki World!";
+        textComponent.fontSize = 24;
+        textComponent.color = Color.white;
+
+        RectTransform rect = textComponent.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(600, 200);
+        rect.anchoredPosition = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateText(string newText)
     {
-        
+        textComponent.text = newText;
     }
+}
 }
--- Assets/Scripts/V8BigBlock/SceneController.cs
+++ Assets/Scripts/V8BigBlock/SceneController.cs
@@ -7,6 +7,7 @@ public class SceneController : MonoBehaviour
     public static SceneController Instance;
     private GameObject background;
     private GameObject monika;
+    private GameObject textBox;
 
     //public Rect t44 = new Rect(100, 100, 100, 100); // Not SDL - Rect and RectTransform are different in Unity
 
@@ -34,30 +35,35 @@ public class SceneController : MonoBehaviour
         // a raw jpg/png needs converting to a texture2D to be used in Resources.Load()
         Texture2D backgroundTexture = Resources.Load<Texture2D>(path);
         Texture2D monikaTexture = Resources.Load<Texture2D>(monikaPath);
+        Texture2D textBoxTexture = Resources.Load<Texture2D>("gui/textbox");
 
-        Debug.Log("Monika texture: " + (monikaTexture != null ? "Loaded" : "Null"));
-
+        
         if (backgroundTexture == null)
         {
             Debug.LogError("Failed to load background texture.");
             return;
         }
-        Sprite sprite = Sprite.Create(backgroundTexture, new Rect(0,0, backgroundTexture.width, backgroundTexture.height), new Vector2(0.5f, 0.5f), 100f);
+        Sprite backgroundSprite = Sprite.Create(backgroundTexture, new Rect(0,0, backgroundTexture.width, backgroundTexture.height), new Vector2(0.5f, 0.5f), 100f);
         Sprite monikaSprite = Sprite.Create(monikaTexture, new Rect(0,0, monikaTexture.width, monikaTexture.height), new Vector2(0.5f, 0.5f), 100f);
-        
+        Sprite textBoxSprite = Sprite.Create(textBoxTexture, new Rect(0,0, textBoxTexture.width, textBoxTexture.height), new Vector2(0.5f, 0.5f), 100f);
+
         background = new GameObject("Background");
         monika = new GameObject("Monika");
+        textBox = new GameObject("TextBox");
 
         SpriteRenderer sr = background.AddComponent<SpriteRenderer>();
-        sr.sprite = sprite;
+        sr.sprite = backgroundSprite;
         sr.sortingOrder = -10; // Background should be behind everything else
 
         SpriteRenderer msr = monika.AddComponent<SpriteRenderer>();
         msr.sprite = monikaSprite;
-        msr.sortingOrder = 10; // Monika should be in front of the background
+        msr.sortingOrder = 5; // Monika should be in front of the background
+
+        SpriteRenderer tsr = textBox.AddComponent<SpriteRenderer>();
+        tsr.sprite = textBoxSprite;
+        tsr.sortingOrder = 10; // TextBox should be in front of Monika
 
         background.transform.position = Vector3.zero;
-        Debug.Log("Monika GO final state: position " + monika.transform.position + ", sprite: " + msr.sprite + ", enabled: " + msr.enabled);
-
+        textBox.transform.position = new Vector3(0, -4, 0);
     }
 }
